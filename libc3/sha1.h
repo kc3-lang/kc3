983294acef0e6b6a51d73ccb963a15b5ca02a0ca
Thomas de Grivel <thoxdg@gmail.com>
2023-01-26 11:30:49 +0100

    sha1.h

--- libc3/sha1.h
+++ libc3/sha1.h
@@ -6,14 +6,11 @@
 #else
 # if HAVE_SHA_H
 #  include <sha.h>
+#  define SHA1_DIGEST_LENGTH SHA_DIGEST_LENGTH
+#  define SHA1Final SHA1_Final
+#  define SHA1Init SHA1_Init
+#  define SHA1Update SHA1_Update
 # endif
-#endif
-
-#if HAVE_SHA_H
-#define SHA1_DIGEST_LENGTH SHA_DIGEST_LENGTH
-#define SHA1Final SHA1_Final
-#define SHA1Init SHA1_Init
-#define SHA1Update SHA1_Update
 #endif
 
 #endif /* LIBC3_SHA1_H */
