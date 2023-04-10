0dac458239e75a057829a35675ee728a08b151f2
Thomas de Grivel <thoxdg@gmail.com>
2023-04-10 21:17:46 +0200

    fix ceiling_s64

--- libc3/ceiling.c
+++ libc3/ceiling.c
@@ -10,29 +10,44 @@
  * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
  * THIS SOFTWARE.
  */
+#include <assert.h>
+#include <err.h>
 #include "abs.h"
 #include "ceiling.h"
 #include "sign.h"
 
-#define DEF_CEILING_S(bits, bits2)                                     \
+#define DEF_CEILING_S(bits)                                            \
   s ## bits ceiling_s ## bits (s ## bits a, s ## bits b)               \
   {                                                                    \
+    s ## bits result;                                                  \
     s ## bits sign;                                                    \
     sign = sign_s ## bits (a) * sign_s ## bits (b);                    \
     a = abs_s ## bits (a);                                             \
     b = abs_s ## bits (b);                                             \
-    return (s ## bits) (sign * ((s ## bits2) a + (b - 1)) / b);        \
+    result = sign * (a / b + ((a % b) ? 1 : 0));                       \
+    return result;                                                     \
   }
 
-#define DEF_CEILING_U(bits, bits2)                                     \
+#define DEF_CEILING_U(bits)                                            \
   u ## bits ceiling_u ## bits (u ## bits a, u ## bits b)               \
   {                                                                    \
-    return (u ## bits) (((u ## bits2) a + (b - 1)) / b);               \
+    u ## bits result;                                                  \
+    if (b == 0) {                                                      \
+      assert(! "ceiling_u" # bits ": division by zero");               \
+      errx(1, "ceiling_u" # bits ": division by zero");                \
+      return 0;                                                        \
+    }                                                                  \
+    result = a / b + ((a % b) ? 1 : 0);                                \
+    return result;                                                     \
   }
 
-DEF_CEILING_S(8, 16)
-DEF_CEILING_S(16, 32)
-DEF_CEILING_S(32, 64)
-DEF_CEILING_U(8, 16)
-DEF_CEILING_U(16, 32)
-DEF_CEILING_U(32, 64)
+DEF_CEILING_S(8)
+DEF_CEILING_S(16)
+DEF_CEILING_S(32)
+DEF_CEILING_S(64)
+DEF_CEILING_S(w)
+DEF_CEILING_U(8)
+DEF_CEILING_U(16)
+DEF_CEILING_U(32)
+DEF_CEILING_U(64)
+DEF_CEILING_U(w)
