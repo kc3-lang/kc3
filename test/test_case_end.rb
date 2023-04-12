ac64c139c84470a306c09a3be2e25f3bbdc4792e
Thomas de Grivel <thoxdg@gmail.com>
2023-04-12 17:49:51 +0200

    test_case_end.rb

---
+++ test/test_case_end.rb
@@ -0,0 +1,15 @@
+Dir["*.c"].each do |file|
+  file_tmp = file + ".tmp"
+  File.open(file_tmp, "w") do |out|
+    test_case_name = nil
+    File.foreach(file) do |line|
+      line.match(/^TEST_CASE\((.*)\)$/) do |m|
+        test_case_name = m[1]
+      end
+      out.write line
+      out.puts "TEST_CASE_END(#{test_case_name})" if line == "}\n"
+    end
+  end
+  FileUtils.cp(file, file + "~")
+  FileUtils.mv(file_tmp, file)
+end
