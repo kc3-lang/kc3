7863cafbd9b832c7c5ed035fb3e9f6fc4ade5a9c
Thomas de Grivel <thoxdg@gmail.com>
2023-08-28 10:16:27 +0200

    asan

--- libc3/buf_parse.c
+++ libc3/buf_parse.c
@@ -466,7 +466,7 @@ sw buf_parse_brackets (s_buf *buf, s_call *dest)
   buf_save_restore_rpos(buf, &save);
   call_clean(&tmp);
  clean:
-  list_delete(dimensions);
+  list_delete_all(dimensions);
   buf_save_clean(buf, &save);
   return r;
 }
