f1593987f708c357be5435babf282676706d82f2
Thomas de Grivel <thoxdg@gmail.com>
2023-11-30 19:19:57 +0100

    wip tests

--- test/buf_inspect_test.c
+++ test/buf_inspect_test.c
@@ -197,7 +197,7 @@ TEST_CASE(buf_inspect_array)
   BUF_INSPECT_TEST_ARRAY("(U8){0}", "(U8) {0}");
   BUF_INSPECT_TEST_ARRAY("(U8){{0, 1}, {2, 3}}",
                          "(U8) {{0, 1}, {2, 3}}");
-  BUF_INSPECT_TEST_ARRAY("(U8) { { { 0 , 1 } , {2 , 3 } } , { { 4 , 5 } , { 6 , 7 } } } }",
+  BUF_INSPECT_TEST_ARRAY("(U8) { { { 0 , 1 } , {2 , 3 } } , { { 4 , 5 } , { 6 , 7 } } }",
                          "(U8) {{{0, 1}, {2, 3}}, {{4, 5}, {6, 7}}}");
 }
 TEST_CASE_END(buf_inspect_array)
