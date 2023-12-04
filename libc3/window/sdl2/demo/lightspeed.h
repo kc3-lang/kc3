2543fb6912b29b576731d7a56efdde39cee15226
Thomas de Grivel <thoxdg@gmail.com>
2023-12-04 02:48:42 +0100

    lightspeed

--- libc3/window/sdl2/demo/lightspeed.h
+++ libc3/window/sdl2/demo/lightspeed.h
@@ -15,7 +15,7 @@
 
 #include "../types.h"
 
-#define LIGHTSPEED_STARS 2000
+#define LIGHTSPEED_STARS 10000
 
 bool lightspeed_load (s_sequence *seq, s_window_sdl2 *window);
 bool lightspeed_render (s_sequence *seq, s_window_sdl2 *window,
