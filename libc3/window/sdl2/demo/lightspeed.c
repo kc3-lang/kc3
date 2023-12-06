753ba0386cd0e17fc94a58051d571846a69ef67e
Thomas de Grivel <thoxdg@gmail.com>
2023-12-06 09:39:47 +0100

    minor tweaks

--- libc3/window/sdl2/demo/lightspeed.c
+++ libc3/window/sdl2/demo/lightspeed.c
@@ -13,8 +13,8 @@
 #include <libc3/c3.h>
 #include "lightspeed.h"
 
-#define LIGHTSPEED_STAR_MAX        1024 * 1024
-#define LIGHTSPEED_STAR_PROBABILITY       0.005
+#define LIGHTSPEED_STAR_MAX (1024 * 1024)
+#define LIGHTSPEED_STAR_PROBABILITY 0.005
 
 static void star_init (s_tag *star)
 {
@@ -24,9 +24,9 @@ static void star_init (s_tag *star)
   f64_random(&y);
   if (star->type != TAG_MAP || star->data.map.count != 3) {
     tag_map(star, 3);
-    tag_init_sym(star->data.map.keys   + 0, sym_1("speed"));
-    tag_init_sym(star->data.map.keys   + 1, sym_1("x"));
-    tag_init_sym(star->data.map.keys   + 2, sym_1("y"));
+    tag_init_sym(star->data.map.keys + 0, sym_1("speed"));
+    tag_init_sym(star->data.map.keys + 1, sym_1("x"));
+    tag_init_sym(star->data.map.keys + 2, sym_1("y"));
   }
   tag_init_f64(star->data.map.values + 0, 0.0);
   tag_init_f64(star->data.map.values + 1, 2.0 * x - 1.0);
