5ebd9385f0f1fb83a49759e0760b58bda2541655
Thomas de Grivel <thoxdg@gmail.com>
2023-12-13 21:53:07 +0100

    ic3 with wineditline

---
+++ ic3/buf_wineditline.h
@@ -0,0 +1,24 @@
+/* c3
+ * Copyright 2022,2023 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+/**
+ * @file buf_wineditline.h
+ * @brief Wineditline backend for s_buf.
+ */
+#ifndef IC3_BUF_WINEDITLINE_H
+#define IC3_BUF_WINEDITLINE_H
+
+void    buf_wineditline_close (s_buf *buf, const s8 *history_path);
+s_buf * buf_wineditline_open_r (s_buf *buf, const s8 *prompt,
+				const s8 *history_path);
+
+#endif /* IC3_BUF_WINEDITLINE_H */
