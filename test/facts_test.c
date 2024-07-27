cdc4416f454bee0280bc4c7b9352b9cf848f4237
Thomas de Grivel <thoxdg@gmail.com>
2024-07-27 20:40:49 +0200

    buf_inspect_s, buf_inspect_u: print cast when necessary

--- test/facts_test.c
+++ test/facts_test.c
@@ -467,7 +467,7 @@ TEST_CASE(facts_open_file)
   }
   facts_init(&facts);
   str_init_1(&path, NULL, "facts_test_open_file.1.facts");
-  TEST_EQ(facts_open_file(&facts, &path), 761);
+  TEST_EQ(facts_open_file(&facts, &path), 944);
   TEST_EQ(facts_count(&facts), 23);
   i = 0;
   while (p[i]) {
