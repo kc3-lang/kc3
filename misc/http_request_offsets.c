ed29f501a2d72a4f947bb2b16aa5119ee3bad4f5
Thomas de Grivel <thoxdg@gmail.com>
2024-08-02 14:28:38 +0200

    misc

--- misc/http_request_offsets.c
+++ misc/http_request_offsets.c
@@ -3,7 +3,6 @@
 #include <assert.h>
 #include "../http/types.h"
 
-// Function to calculate and print offsets
 void print_offsets() {
     printf("Offset of method: %zu\n", offsetof(struct http_request, method));
     printf("Offset of url: %zu\n", offsetof(struct http_request, url));
@@ -11,10 +10,7 @@ void print_offsets() {
     printf("Offset of headers: %zu\n", offsetof(struct http_request, headers));
 }
 
-// Main function
-int main() {
-    // Print offsets
-    print_offsets();
-
-    return 0;
+int main (void) {
+  print_offsets();
+  return 0;
 }
