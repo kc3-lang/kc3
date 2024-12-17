2346faac9500524ecd2d98fedcfe4ad61a32d3c7
Thomas de Grivel <thoxdg@gmail.com>
2024-12-17 12:22:40 +0100

    dlopen

---
+++ gtk4/object.c
@@ -0,0 +1,18 @@
+/* kc3
+ * Copyright 2022,2023,2024 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include "object.h"
+
+void kc3_gtk4_object_unref (GObject **object)
+{
+  g_object_unref(G_OBJECT(*object));
+}
