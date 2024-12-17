49074fd007b8ab7d6903d15ae9a89721999a7fde
Thomas de Grivel <thoxdg@gmail.com>
2024-12-17 11:54:25 +0100

    gtk4: g_menu

---
+++ gtk4/menu.c
@@ -0,0 +1,27 @@
+/* kc3
+ * Copyright 2022,2023,2024 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include "menu.h"
+
+void kc3_gtk4_menu_append_item (GMenu **menu, GMenuItem **item)
+{
+  g_menu_append_item(*menu, *item);
+}
+
+GMenu ** kc3_gtk4_menu_new (GMenu **dest)
+{
+  GMenu *tmp;
+  if (! (tmp = g_menu_new()))
+    return NULL;
+  *dest = tmp;
+  return dest;
+}
