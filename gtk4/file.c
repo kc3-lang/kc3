ed2b5e37cf3c5123908c21d611fa53019b3561d2
Thomas de Grivel <thoxdg@gmail.com>
2024-12-17 12:12:46 +0100

    gtk4: g_file

---
+++ gtk4/file.c
@@ -0,0 +1,23 @@
+/* kc3
+ * Copyright 2022,2023,2024 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include "file.h"
+
+GFile ** kc3_gtk4_file_new_for_uri (GFile **dest,
+                                    const s_str *uri)
+{
+  GFile *tmp;
+  if (! (tmp = g_file_new_for_uri(uri->ptr.pchar)))
+    return NULL;
+  *dest = tmp;
+  return dest;
+}
---
+++ gtk4/file.h
@@ -0,0 +1,24 @@
+/* kc3
+ * Copyright 2022,2023,2024 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#ifndef KC3_GTK4_FILE_H
+#define KC3_GTK4_FILE_H
+
+#include "types.h"
+
+/* Heap-allocation functions. */
+GFile ** kc3_gtk4_file_new_for_uri (GFile **dest,
+                                    const s_str *uri);
+
+/* Operators. */
+
+#endif /* KC3_GTK4_FILE_H */
