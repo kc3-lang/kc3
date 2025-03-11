7b8536d5c4b6a36db484f702542ff695499bcc5f
Thomas de Grivel <thoxdg@gmail.com>
2025-03-11 11:02:52 +0100

    fix env_clean segv

--- libkc3/env.c
+++ libkc3/env.c
@@ -283,9 +283,10 @@ void env_clean (s_env *env)
   env->path = NULL;
   list_delete_all(env->search_modules_default);
   env->search_modules_default = NULL;
-  if (g_kc3_env_default == env)
+  if (g_kc3_env_default == env) {
     sym_delete_all();
-  g_kc3_env_default = NULL;
+    g_kc3_env_default = NULL;
+  }
   if (g_kc3_env_global == env) {
     if (env->parent_env)
       g_kc3_env_global = env->parent_env;
