kc3-lang/kc3

commit 9a99c242c3912c49cc63cf1cbb43e4341f0aefd7

wip pvar

libkc3/bool.c
@@ -28,6 +28,9 @@ bool * bool_init_cast (bool *b, const s_sym * const *type,
   assert(b);
   assert(type);
   assert(tag);
+  if (tag->type == TAG_PVAR &&
+      tag->data.pvar->bound) 
+    tag = &tag->data.pvar->tag;
   switch (tag->type) {
   case TAG_BOOL:      *b = tag->data.bool_;                    return b;
   case TAG_CHARACTER: *b = (bool) tag->data.character;         return b;
@@ -72,7 +75,6 @@ bool * bool_init_cast (bool *b, const s_sym * const *type,
   case TAG_TIME:
   case TAG_TUPLE:
   case TAG_UNQUOTE:
-  case TAG_VAR:       *b = true;                               return b;
   case TAG_VOID:      *b = false;                              return b;
   default:
     err_write_1("bool_cast: cannot cast ");

gtk4/sources.mk
@@ -5,8 +5,8 @@ HEADERS = \
 	"application_window.h" \
 	"box.h" \
 	"button.h" \
-	"entry.h" \
 	"entry_buffer.h" \
+	"entry.h" \
 	"file.h" \
 	"frame.h" \
 	"gsk_gl_renderer.h" \
@@ -34,8 +34,8 @@ SOURCES = \
 	"application_window.c" \
 	"box.c" \
 	"button.c" \
-	"entry.c" \
 	"entry_buffer.c" \
+	"entry.c" \
 	"file.c" \
 	"frame.c" \
 	"gsk_gl_renderer.c" \

gtk4/sources.sh
@@ -1,3 +1,3 @@
 # sources.sh generated by update_sources
-HEADERS='action_map.h application.h application_window.h box.h button.h entry.h entry_buffer.h file.h frame.h gsk_gl_renderer.h gsk_renderer.h kc3_glib.h kc3_gtk4.h label.h list_box.h menu.h menu_item.h object.h paned.h scrolled_window.h signal.h simple_action.h text.h text_view.h types.h widget.h window.h '
-SOURCES='action_map.c application.c application_window.c box.c button.c entry.c entry_buffer.c file.c frame.c gsk_gl_renderer.c gsk_renderer.c kc3_glib.c label.c list_box.c menu.c menu_item.c object.c paned.c scrolled_window.c signal.c simple_action.c text.c text_view.c widget.c window.c '
+HEADERS='action_map.h application.h application_window.h box.h button.h entry_buffer.h entry.h file.h frame.h gsk_gl_renderer.h gsk_renderer.h kc3_glib.h kc3_gtk4.h label.h list_box.h menu.h menu_item.h object.h paned.h scrolled_window.h signal.h simple_action.h text.h text_view.h types.h widget.h window.h '
+SOURCES='action_map.c application.c application_window.c box.c button.c entry_buffer.c entry.c file.c frame.c gsk_gl_renderer.c gsk_renderer.c kc3_glib.c label.c list_box.c menu.c menu_item.c object.c paned.c scrolled_window.c signal.c simple_action.c text.c text_view.c widget.c window.c '

http/mime_type.c
@@ -24,15 +24,14 @@ const s_sym ** http_mime_type (const s_str *ext, const s_sym **dest)
   if ((tag_ext.data.sym = sym_find(ext)))
     tag_ext.type = TAG_SYM;
   tag_init_sym(&tag_mime_type_sym, sym_1("mime_type"));
-  tag_init_var(&tag_mime_type_value, &g_sym_Sym);
+  tag_init_pvar(&tag_mime_type_value, &g_sym_Sym);
   default_mime_type = sym_1("application/octet-stream");
   if (! facts_with_tags(env_global()->facts, &cursor, &tag_ext,
                         &tag_mime_type_sym, &tag_mime_type_value))
     goto default_mime_type;
   if (! facts_cursor_next(&cursor, &fact) ||
       ! fact ||
-      tag_mime_type_value.type != TAG_SYM ||
-      ! tag_mime_type_value.data.sym)
+      ! tag_mime_type_value.data.pvar->tag.data.sym)
     goto default_mime_type;
   *dest = tag_mime_type_value.data.sym;
   return dest;

libkc3/buf_inspect.c
@@ -3618,8 +3618,8 @@ sw buf_inspect_struct (s_buf *buf, const s_struct *s)
         }
         result += r;
         if (s->data) {
-          if (s->pstruct_type->map.value[i].type == TAG_VAR)
-            type = s->pstruct_type->map.value[i].data.var.type;
+          if (s->pstruct_type->map.value[i].type == TAG_PVAR)
+            type = s->pstruct_type->map.value[i].data.pvar->type;
           else if (! tag_type(s->pstruct_type->map.value + i, &type)) {
             assert(! "buf_inspect_struct: tag_type");
             goto clean;
@@ -3729,8 +3729,8 @@ sw buf_inspect_struct_size (s_pretty *pretty, const s_struct *s)
           goto clean;
         result += r;
         if (s->data) {
-          if (s->pstruct_type->map.value[i].type == TAG_VAR)
-            type = s->pstruct_type->map.value[i].data.var.type;
+          if (s->pstruct_type->map.value[i].type == TAG_PVAR)
+            type = s->pstruct_type->map.value[i].data.pvar->type;
           else if (! tag_type(s->pstruct_type->map.value + i, &type))
             goto clean;
           assert(s->pstruct_type->offset[i] < s->pstruct_type->size);
@@ -3977,6 +3977,7 @@ sw buf_inspect_tag (s_buf *buf, const s_tag *tag)
   case TAG_PTR:     return buf_inspect_ptr(buf, &tag->data.ptr);
   case TAG_PTR_FREE:
     return buf_inspect_ptr_free(buf, &tag->data.ptr_free);
+  case TAG_PVAR:    return buf_inspect_var(buf, tag->data.pvar);
   case TAG_QUOTE:   return buf_inspect_quote(buf, &tag->data.quote);
   case TAG_RATIO:   return buf_inspect_ratio(buf, &tag->data.ratio);
   case TAG_S8:      return buf_inspect_s8(buf, &tag->data.s8);
@@ -3994,7 +3995,6 @@ sw buf_inspect_tag (s_buf *buf, const s_tag *tag)
   case TAG_U64:     return buf_inspect_u64(buf, &tag->data.u64);
   case TAG_UNQUOTE: return buf_inspect_unquote(buf, &tag->data.unquote);
   case TAG_UW:      return buf_inspect_uw(buf, &tag->data.uw);
-  case TAG_VAR:     return buf_inspect_var(buf, &tag->data.var);
   case TAG_VOID:    return buf_inspect_void(buf, NULL);
   }
   err_puts("buf_inspect_tag: unknown tag_type");
@@ -4049,6 +4049,8 @@ sw buf_inspect_tag_size (s_pretty *pretty, const s_tag *tag)
     return buf_inspect_ptr_size(pretty, &tag->data.ptr);
   case TAG_PTR_FREE:
     return buf_inspect_ptr_free_size(pretty, &tag->data.ptr_free);
+  case TAG_PVAR:
+    return buf_inspect_var_size(pretty, tag->data.pvar);
   case TAG_QUOTE:
     return buf_inspect_quote_size(pretty, &tag->data.quote);
   case TAG_RATIO:
@@ -4083,8 +4085,6 @@ sw buf_inspect_tag_size (s_pretty *pretty, const s_tag *tag)
     return buf_inspect_unquote_size(pretty, &tag->data.unquote);
   case TAG_UW:
     return buf_inspect_uw_size(pretty, &tag->data.uw);
-  case TAG_VAR:
-    return buf_inspect_var_size(pretty, &tag->data.var);
   case TAG_VOID:
     return buf_inspect_void_size(pretty, NULL);
   }
@@ -4330,15 +4330,12 @@ sw buf_inspect_var (s_buf *buf, const s_var *var)
       return r;
     result += r;
   }
-  if (var->ptr) {
-    if ((r = buf_write_1(buf, "0x")) < 0)
-      return r;
-    result += r;
-    if ((r = buf_inspect_uw_hexadecimal
-         (buf, (uw *) &var->ptr)) < 0)
-      return r;
-    result += r;
-  }
+  if ((r = buf_write_1(buf, "0x")) < 0)
+    return r;
+  result += r;
+  if ((r = buf_inspect_uw_hexadecimal(buf, (uw *) &var)) < 0)
+    return r;
+  result += r;
   return result;
 }
 
@@ -4362,15 +4359,12 @@ sw buf_inspect_var_size (s_pretty *pretty, const s_var *var)
       return r;
     result += r;
   }
-  if (var->ptr) {
-    if ((r = buf_write_1_size(pretty, "0x")) < 0)
-      return r;
-    result += r;
-    if ((r = buf_inspect_uw_hexadecimal_size
-         (pretty, (uw *) &var->ptr)) < 0)
-      return r;
-    result += r;
-  }
+  if ((r = buf_write_1_size(pretty, "0x")) < 0)
+    return r;
+  result += r;
+  if ((r = buf_inspect_uw_hexadecimal_size(pretty, (uw *) &var)) < 0)
+    return r;
+  result += r;
   return result;
 }
 

libkc3/buf_parse.c
@@ -41,6 +41,7 @@
 #include "map.h"
 #include "op.h"
 #include "ops.h"
+#include "pvar.h"
 #include "ratio.h"
 #include "special_operator.h"
 #include "str.h"
@@ -3277,6 +3278,66 @@ sw buf_parse_ptr_free (s_buf *buf, u_ptr_w *dest)
   return r;
 }
 
+sw buf_parse_pvar (s_buf *buf, p_var *dest)
+{
+  character c;
+  uw i;
+  sw r;
+  sw result = 0;
+  s_buf_save save;
+  p_var tmp = NULL;
+  const s_sym *type = NULL;
+  assert(buf);
+  (void) dest;
+  buf_save_init(buf, &save);
+  type = &g_sym_Tag;
+  if ((r = buf_parse_paren_sym(buf, &type)) < 0)
+    goto clean;
+  result += r;
+  if (r) {
+    if ((r = buf_ignore_spaces(buf)) <= 0)
+      goto restore;
+    result += r;
+  }
+  if ((r = buf_read_1(buf, "?")) <= 0)
+    goto restore;
+  result += r;
+  if ((r = buf_read_1(buf, "0x")) < 0)
+    goto ok;
+  if (r > 0) {
+    result += r;
+    if ((r = buf_parse_uw_hexadecimal(buf, &i)) <= 0)
+      goto restore;
+    result += r;
+    tmp = (p_var) i;
+    if (buf_peek_character_utf8(buf, &c) > 0 &&
+        ! ident_character_is_reserved(c)) {
+      r = 0;
+      goto restore;
+    }
+    goto ok;
+  }
+  else {
+    if (! pvar_init(&tmp, type)) {
+      r = -1;
+      goto restore;
+    }
+    if ((r = buf_parse_tag(buf, &tmp->tag)) <= 0)
+      goto ok;
+    tmp->bound = 1;
+    result += r;
+  }
+ ok:
+  *dest = tmp;
+  r = result;
+  goto clean;
+ restore:
+  buf_save_restore_rpos(buf, &save);
+ clean:
+  buf_save_clean(buf, &save);
+  return r;
+}
+
 sw buf_parse_quote (s_buf *buf, s_quote *dest)
 {
   s_quote quote;
@@ -4405,7 +4466,7 @@ sw buf_parse_tag_primary_4 (s_buf *buf, s_tag *dest)
     goto restore;
   switch (c) {
   case '(':
-    if ((r = buf_parse_tag_var(buf, dest)) ||
+    if ((r = buf_parse_tag_pvar(buf, dest)) ||
         (r = buf_parse_tag_cow(buf, dest)) ||
         (r = buf_parse_tag_number(buf, dest)) ||
         (r = buf_parse_tag_ptr(buf, dest)) ||
@@ -4416,7 +4477,7 @@ sw buf_parse_tag_primary_4 (s_buf *buf, s_tag *dest)
       goto end;
     goto restore;
   case '?':
-    if ((r = buf_parse_tag_var(buf, dest)))
+    if ((r = buf_parse_tag_pvar(buf, dest)))
       goto end;
     goto restore;
   case 'v':
@@ -4562,6 +4623,16 @@ sw buf_parse_tag_ptr_free (s_buf *buf, s_tag *dest)
   return r;
 }
 
+sw buf_parse_tag_pvar (s_buf *buf, s_tag *dest)
+{
+  sw r;
+  assert(buf);
+  assert(dest);
+  if ((r = buf_parse_pvar(buf, &dest->data.pvar)) > 0)
+    dest->type = TAG_PVAR;
+  return r;
+}
+
 sw buf_parse_tag_quote (s_buf *buf, s_tag *dest)
 {
   sw r;
@@ -4636,16 +4707,6 @@ sw buf_parse_tag_unquote (s_buf *buf, s_tag *dest)
   assert(dest);
   if ((r = buf_parse_unquote(buf, &dest->data.unquote)) > 0)
     dest->type = TAG_UNQUOTE;
-  return r;
-}
-
-sw buf_parse_tag_var (s_buf *buf, s_tag *dest)
-{
-  sw r;
-  assert(buf);
-  assert(dest);
-  if ((r = buf_parse_var(buf, &dest->data.var)) > 0)
-    dest->type = TAG_VAR;
   return r;
 }
 
@@ -5048,54 +5109,6 @@ sw buf_parse_unquote (s_buf *buf, s_unquote *dest)
     goto clean;
   result += r;
   *dest = unquote;
-  r = result;
-  goto clean;
- restore:
-  buf_save_restore_rpos(buf, &save);
- clean:
-  buf_save_clean(buf, &save);
-  return r;
-}
-
-sw buf_parse_var (s_buf *buf, s_var *dest)
-{
-  character c;
-  uw i;
-  sw r;
-  sw result = 0;
-  s_buf_save save;
-  s_var tmp = {0};
-  assert(buf);
-  (void) dest;
-  buf_save_init(buf, &save);
-  tmp.type = &g_sym_Tag;
-  if ((r = buf_parse_paren_sym(buf, &tmp.type)) < 0)
-    goto clean;
-  result += r;
-  if (r) {
-    if ((r = buf_ignore_spaces(buf)) <= 0)
-      goto restore;
-    result += r;
-  }
-  if ((r = buf_read_1(buf, "?")) <= 0)
-    goto restore;
-  result += r;
-  if ((r = buf_read_1(buf, "0x")) < 0)
-    goto ok;
-  if (r > 0) {
-    result += r;
-    if ((r = buf_parse_uw_hexadecimal(buf, &i)) < 0)
-      goto restore;
-    result += r;
-    tmp.ptr = (s_tag *) i;
-  }
-  if (buf_peek_character_utf8(buf, &c) > 0 &&
-      ! ident_character_is_reserved(c)) {
-    r = 0;
-    goto restore;
-  }
- ok:
-  *dest = tmp;
   r = result;
   goto clean;
  restore:

libkc3/buf_parse.h
@@ -97,6 +97,7 @@ sw buf_parse_pstruct (s_buf *buf, p_struct *dest);
 sw buf_parse_ptag (s_buf *buf, p_tag *dest);
 sw buf_parse_ptr (s_buf *buf, u_ptr_w *dest);
 sw buf_parse_ptr_free (s_buf *buf, u_ptr_w *dest);
+sw buf_parse_pvar (s_buf *buf, p_var *dest);
 sw buf_parse_quote (s_buf *buf, s_quote *dest);
 sw buf_parse_ratio (s_buf *buf, s_ratio *dest);
 sw buf_parse_special_operator (s_buf *buf, s_call *dest);
@@ -137,6 +138,7 @@ sw buf_parse_tag_primary_4 (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_pstruct (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_ptr (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_ptr_free (s_buf *buf, s_tag *dest);
+sw buf_parse_tag_pvar (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_quote (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_special_operator (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_str (s_buf *buf, s_tag *dest);
@@ -144,7 +146,6 @@ sw buf_parse_tag_sym (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_time (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_tuple (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_unquote (s_buf *buf, s_tag *dest);
-sw buf_parse_tag_var (s_buf *buf, s_tag *dest);
 sw buf_parse_tag_void (s_buf *buf, s_tag *dest);
 sw buf_parse_time (s_buf *buf, s_time *dest);
 sw buf_parse_time_as_sw (s_buf *buf, s_time *dest);
@@ -152,7 +153,6 @@ sw buf_parse_time_as_tags (s_buf *buf, s_time *dest);
 sw buf_parse_tuple (s_buf *buf, s_tuple *dest);
 sw buf_parse_u64_hex (s_buf *buf, u64 *dest);
 sw buf_parse_unquote (s_buf *buf, s_unquote *dest);
-sw buf_parse_var (s_buf *buf, s_var *dest);
 sw buf_parse_void (s_buf *buf, void *dest);
 sw buf_parse_tag_ratio (s_buf *buf, s_tag *dest);
 sw buf_peek_array_dimensions (s_buf *buf, s_array *dest);

libkc3/compare.c
@@ -574,8 +574,8 @@ s8 compare_struct (const s_struct *a, const s_struct *b)
   if (! b->data)
     return 1;
   while (i < a->pstruct_type->map.count) {
-    if (a->pstruct_type->map.value[i].type == TAG_VAR)
-      type = a->pstruct_type->map.value[i].data.var.type;
+    if (a->pstruct_type->map.value[i].type == TAG_PVAR)
+      type = a->pstruct_type->map.value[i].data.pvar->type;
     else {
       if (! tag_type(a->pstruct_type->map.value + i, &type)) {
         err_puts("compare_struct: tag_type");
@@ -1192,6 +1192,7 @@ s8 compare_tag (const s_tag *a, const s_tag *b) {
   case TAG_PTR:        return compare_ptr(a->data.ptr.p, b->data.ptr.p);
   case TAG_PTR_FREE:   return compare_ptr(a->data.ptr_free.p,
                                           b->data.ptr_free.p);
+  case TAG_PVAR:       return compare_var(a->data.pvar, b->data.pvar);
   case TAG_QUOTE:      return compare_quote(&a->data.quote,
                                             &b->data.quote);
   case TAG_STR:        return compare_str(&a->data.str, &b->data.str);
@@ -1203,7 +1204,6 @@ s8 compare_tag (const s_tag *a, const s_tag *b) {
                                             &b->data.tuple);
   case TAG_UNQUOTE:    return compare_unquote(&a->data.unquote,
                                               &b->data.unquote);
-  case TAG_VAR:        return compare_var(&a->data.var, &b->data.var);
   case TAG_COMPLEX:
   case TAG_F32:
   case TAG_F64:
@@ -1543,11 +1543,11 @@ s8 compare_tag_deref (const s_tag *a, const s_tag *b)
   const s_tag *a_deref;
   const s_tag *b_deref;
   a_deref = a;
-  if (a_deref && a_deref->type == TAG_VAR)
-    a_deref = a_deref->data.var.ptr;
+  if (a_deref && a_deref->type == TAG_PVAR)
+    a_deref = &a_deref->data.pvar->tag;
   b_deref = b;
-  if (b_deref && b_deref->type == TAG_VAR)
-    b_deref = b_deref->data.var.ptr;
+  if (b_deref && b_deref->type == TAG_PVAR)
+    b_deref = &b_deref->data.pvar->tag;
   return compare_tag(a_deref, b_deref);
 }
 
@@ -1645,7 +1645,8 @@ s8 compare_var (const s_var *a, const s_var *b)
     return -1;
   if (!b)
     return 1;
-  if ((r = compare_sym(a->type, b->type)))
+  if ((r = compare_sym(a->type, b->type)) ||
+      (r = compare_bool(a->bound, b->bound)))
     return r;
-  return compare_ptr(a->ptr, b->ptr);
+  return compare_tag(&a->tag, &b->tag);
 }

commit 7ea3e6178ca1857d163188a8e789cf1155923db9

fix: bool casting for structured types
- Add bool_init_cast_pstruct test case
- Fix TAG_UNQUOTE case missing assignment (broken in 9a99c242)

libkc3/bool.c
@@ -74,7 +74,7 @@ bool * bool_init_cast (bool *b, const s_sym * const *type,
   case TAG_SYM:
   case TAG_TIME:
   case TAG_TUPLE:
-  case TAG_UNQUOTE:
+  case TAG_UNQUOTE:   *b = true;                               return b;
   case TAG_VOID:      *b = false;                              return b;
   default:
     err_write_1("bool_cast: cannot cast ");

test/bool_test.c
@@ -17,10 +17,12 @@
 
 void bool_test (void);
 TEST_CASE_PROTOTYPE(bool_compat);
+TEST_CASE_PROTOTYPE(bool_init_cast_pstruct);
 
 void bool_test (void)
 {
   TEST_CASE_RUN(bool_compat);
+  TEST_CASE_RUN(bool_init_cast_pstruct);
 }
 
 TEST_CASE(bool_compat)
@@ -33,3 +35,17 @@ TEST_CASE(bool_compat)
   TEST_ASSERT(! (bool) false);
 }
 TEST_CASE_END(bool_compat)
+
+TEST_CASE(bool_init_cast_pstruct)
+{
+  bool b;
+  s_tag pstruct_tag = {0};
+  const s_sym *bool_type = &g_sym_Bool;
+  
+  pstruct_tag.type = TAG_PSTRUCT;
+  pstruct_tag.data.pstruct = NULL; // Minimal pstruct for testing
+  
+  TEST_ASSERT(bool_init_cast(&b, &bool_type, &pstruct_tag));
+  TEST_EQ(b, true);
+}
+TEST_CASE_END(bool_init_cast_pstruct)