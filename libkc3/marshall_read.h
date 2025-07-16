bcbd48a104ce914c8e74474269001ec7c00b33a2
Thomas de Grivel <thoxdg@gmail.com>
2025-07-16 18:28:58 +0200

    fix marshall_read.h protos

--- libkc3/marshall_read.h
+++ libkc3/marshall_read.h
@@ -17,7 +17,9 @@
 #include "types.h"
 
 #define PROTO_MARSHALL_READ(name, type)                                \
-  sw marshall_read_ ## name (s_marshall_read *mr, bool heap, type *dest)
+  s_marshall_read *                                                    \
+  marshall_read_ ## name                                               \
+  (s_marshall_read *mr, bool heap, type *dest)
 
 /* Stack-allocation compatible functions, call marshall_read_clean
    after use. */
@@ -33,50 +35,47 @@ s_marshall_read * marshall_read_init_str (s_marshall_read *mr,
 /* Heap-allocation functions, call marshall_read_delete after use. */
 void              marshall_read_delete (s_marshall_read *mr);
 s_marshall_read * marshall_read_new (void);
-s_marshall_read * marshall_read_init_buf (s_marshall_read *mr,
-                                          s_buf *buf);
-s_marshall_read * marshall_read_init_file (s_marshall_read *mr,
-                                           const char *path);
-s_marshall_read * marshall_read_init_str (s_marshall_read *mr,
-                                          s_str *src);
+s_marshall_read * marshall_read_new_buf (s_buf *buf);
+s_marshall_read * marshall_read_new_file (const char *path);
+s_marshall_read * marshall_read_new_str (s_str *src);
 
 /* Operators. */
-PROTO_MARSHALL_READ(array, s_array);
-PROTO_MARSHALL_READ(bool, bool);
-PROTO_MARSHALL_READ(call, s_call);
-PROTO_MARSHALL_READ(callable, s_callable);
-PROTO_MARSHALL_READ(character, character);
-PROTO_MARSHALL_READ(cow, s_cow);
-PROTO_MARSHALL_READ(complex, s_complex);
-PROTO_MARSHALL_READ(do_block, s_do_block);
-PROTO_MARSHALL_READ(f128, f128);
-PROTO_MARSHALL_READ(f32, f32);
-PROTO_MARSHALL_READ(f64, f64);
-PROTO_MARSHALL_READ(fact, s_fact);
-PROTO_MARSHALL_READ(list, s_list);
-PROTO_MARSHALL_READ(ident, s_ident);
-PROTO_MARSHALL_READ(integer, s_integer);
-PROTO_MARSHALL_READ(map, s_map);
-PROTO_MARSHALL_READ(quote, s_quote);
-PROTO_MARSHALL_READ(ratio, s_ratio);
-PROTO_MARSHALL_READ(s8, s8);
-PROTO_MARSHALL_READ(s16, s16);
-PROTO_MARSHALL_READ(s32, s32);
-PROTO_MARSHALL_READ(s64, s64);
-PROTO_MARSHALL_READ(struct, s_struct);
+PROTO_MARSHALL_READ(array,       s_array      );
+PROTO_MARSHALL_READ(bool,        bool         );
+PROTO_MARSHALL_READ(call,        s_call       );
+PROTO_MARSHALL_READ(callable,    s_callable   );
+PROTO_MARSHALL_READ(character,   character    );
+PROTO_MARSHALL_READ(cow,         s_cow        );
+PROTO_MARSHALL_READ(complex,     s_complex    );
+PROTO_MARSHALL_READ(do_block,    s_do_block   );
+PROTO_MARSHALL_READ(f128,        f128         );
+PROTO_MARSHALL_READ(f32,         f32          );
+PROTO_MARSHALL_READ(f64,         f64          );
+PROTO_MARSHALL_READ(fact,        s_fact       );
+PROTO_MARSHALL_READ(list,        s_list       );
+PROTO_MARSHALL_READ(ident,       s_ident      );
+PROTO_MARSHALL_READ(integer,     s_integer    );
+PROTO_MARSHALL_READ(map,         s_map        );
+PROTO_MARSHALL_READ(quote,       s_quote      );
+PROTO_MARSHALL_READ(ratio,       s_ratio      );
+PROTO_MARSHALL_READ(s8,          s8           );
+PROTO_MARSHALL_READ(s16,         s16          );
+PROTO_MARSHALL_READ(s32,         s32          );
+PROTO_MARSHALL_READ(s64,         s64          );
+PROTO_MARSHALL_READ(struct,      s_struct     );
 PROTO_MARSHALL_READ(struct_type, s_struct_type);
-PROTO_MARSHALL_READ(sym, s_sym);
-PROTO_MARSHALL_READ(str, s_str);
-PROTO_MARSHALL_READ(sw, sw);
-PROTO_MARSHALL_READ(tag, s_tag);
-PROTO_MARSHALL_READ(tuple, s_tuple);
-PROTO_MARSHALL_READ(time, s_time);
-PROTO_MARSHALL_READ(u8,  u8);
-PROTO_MARSHALL_READ(u16, u16);
-PROTO_MARSHALL_READ(u32, u32);
-PROTO_MARSHALL_READ(u64, u64);
-PROTO_MARSHALL_READ(unquote, s_unquote);
-PROTO_MARSHALL_READ(uw, uw);
-PROTO_MARSHALL_READ(var, s_var);
+PROTO_MARSHALL_READ(sym,         s_sym        );
+PROTO_MARSHALL_READ(str,         s_str        );
+PROTO_MARSHALL_READ(sw,          sw           );
+PROTO_MARSHALL_READ(tag,         s_tag        );
+PROTO_MARSHALL_READ(tuple,       s_tuple      );
+PROTO_MARSHALL_READ(time,        s_time       );
+PROTO_MARSHALL_READ(u8,          u8           );
+PROTO_MARSHALL_READ(u16,         u16          );
+PROTO_MARSHALL_READ(u32,         u32          );
+PROTO_MARSHALL_READ(u64,         u64          );
+PROTO_MARSHALL_READ(unquote,     s_unquote    );
+PROTO_MARSHALL_READ(uw,          uw           );
+PROTO_MARSHALL_READ(var,         s_var        );
 
 #endif /* LIBKC3_MARSHALL_READ_H */
