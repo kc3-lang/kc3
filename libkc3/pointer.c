b046bf18a1d16c98f4be6be55905f12a4adfd888
Perle <56500403+lpinf-code@users.noreply.github.com>
2025-08-22 16:15:46 +0200

    feat: pointer_init_copy and pointer.c

---
+++ libkc3/pointer.c
@@ -0,0 +1,23 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include "assert.h"
+#include "pointer.h"
+
+s_pointer * pointer_init_copy (s_pointer *pointer,
+                               const s_pointer *src)
+{
+  assert(pointer);
+  assert(src);
+  *pointer = *src;
+  return pointer;
+}
