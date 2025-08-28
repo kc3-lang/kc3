b8b6ed4b3a235308cee894bf0932f2921dccc640
Thomas de Grivel <thoxdg@gmail.com>
2025-08-28 15:04:14 +0200

    wip tls certificate path

--- tls/tls.c
+++ tls/tls.c
@@ -10,9 +10,38 @@
  * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
  * THIS SOFTWARE.
  */
+#ifdef _WIN32
+#include <windows.h>
+#include <wincrypt.h>
+#else
+#include <unistd.h>
+#include <stdlib.h>
+#endif
 #include <tls.h>
 #include <libkc3/kc3.h>
 #include "tls.h"
+
+const char * kc3_tls_get_ca_cert_path (void)
+{
+  static const char* ca_cert_paths[] = {
+    "/etc/ssl/certs/ca-certificates.crt",
+    "/etc/pki/tls/certs/ca-bundle.crt",
+    "/etc/ssl/cert.pem",
+    "/usr/local/share/certs/ca-root-nss.crt",
+    "/etc/certs/ca-certificates.crt",
+    NULL
+  };
+  const char *cert_file = getenv("SSL_CERT_FILE");
+  if (cert_file && access(cert_file, R_OK) == 0) {
+    return cert_file;
+  }
+  for (int i = 0; ca_cert_paths[i]; i++) {
+    if (access(ca_cert_paths[i], R_OK) == 0) {
+      return ca_cert_paths[i];
+    }
+  }
+  return NULL;
+}
 
 s_pointer * kc3_tls_config_new (s_pointer *dest)
 {
