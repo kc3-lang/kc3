edcff8a49b5ea3a88d020350893e644a8b469bdd
Thomas de Grivel <thoxdg@gmail.com>
2025-09-01 20:20:06 +0200

    securelevel

---
+++ libkc3/securelevel.c
@@ -0,0 +1,23 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include "securelevel.h"
+
+u8 securelevel (u8 level)
+{
+  static u8 securelevel = 0;
+  if (level > 2)
+    level = 2;
+  if (level > securelevel)
+    securelevel = level;
+  return securelevel;
+}
---
+++ libkc3/securelevel.h
@@ -0,0 +1,20 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#ifndef LIBKC3_SECURELEVEL_H
+#define LIBKC3_SECURELEVEL_H
+
+#include "types.h"
+
+u8 securelevel (u8 level);
+
+#endif /* LIBKC3_SECURELEVEL_H */
