e67bea47850daeb3537305eeefc7a9669a7ee3ed
Thomas de Grivel <thoxdg@gmail.com>
2025-09-18 13:26:46 +0200

    fix typing of pdf_parse_from_file

--- pdf/pdf.h
+++ pdf/pdf.h
@@ -16,7 +16,7 @@
 #include "types.h"
 
 /* Observers. */
-s_pdf_file * pdf_parse_file (s_str *path, s_pdf_file *dest);
-s_tag *      pdf_parse_from_str (s_str *str, s_tag *dest);
+s_pdf_file ** pdf_parse_file (s_str *path, s_pdf_file **dest);
+s_tag *       pdf_parse_from_str (s_str *str, s_tag *dest);
 
 #endif /* KC3_PDF_H */
