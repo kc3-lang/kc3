57f78d34d113f0d63c2247116b554f450b0fa72c
Thomas de Grivel <thoxdg@gmail.com>
2025-09-30 22:42:24 +0200

    workaround bool bug

--- window/egl/android/demo/window_egl_android_demo.c
+++ window/egl/android/demo/window_egl_android_demo.c
@@ -31,12 +31,12 @@ s_gl_ortho g_ortho = {0};
 s_gl_text g_text_fps = {0};
 s_gl_text g_text_seq_title = {0};
 
-static bool window_egl_demo_button (s_window_egl *window, u8 button,
+static u8 window_egl_demo_button (s_window_egl *window, u8 button,
                                     s64 x, s64 y);
-static bool window_egl_demo_key (s_window_egl *window, u32 keysym);
-static bool window_egl_demo_load (s_window_egl *window);
-static bool window_egl_demo_render (s_window_egl *window);
-static bool window_egl_demo_resize (s_window_egl *window, u64 w, u64 h);
+static u8 window_egl_demo_key (s_window_egl *window, u32 keysym);
+static u8 window_egl_demo_load (s_window_egl *window);
+static u8 window_egl_demo_render (s_window_egl *window);
+static u8 window_egl_demo_resize (s_window_egl *window, u64 w, u64 h);
 static void window_egl_demo_unload (s_window_egl *window);
 
 int main (int argc, char **argv)
@@ -71,7 +71,7 @@ int main (int argc, char **argv)
   return 0;
 }
 
-bool window_egl_demo_button (s_window_egl *window, u8 button,
+u8 window_egl_demo_button (s_window_egl *window, u8 button,
                              s64 x, s64 y)
 {
   assert(window);
@@ -89,7 +89,7 @@ bool window_egl_demo_button (s_window_egl *window, u8 button,
   return true;
 }
 
-bool window_egl_demo_key (s_window_egl *window, u32 keysym)
+u8 window_egl_demo_key (s_window_egl *window, u32 keysym)
 {
   assert(window);
   (void) window;
@@ -106,7 +106,7 @@ bool window_egl_demo_key (s_window_egl *window, u32 keysym)
   return true;
 }
 
-bool window_egl_demo_load (s_window_egl *window)
+u8 window_egl_demo_load (s_window_egl *window)
 {
   f32 point_per_pixel;
   assert(window);
@@ -143,7 +143,7 @@ bool window_egl_demo_load (s_window_egl *window)
   return true;
 }
 
-bool window_egl_demo_render (s_window_egl *window)
+u8 window_egl_demo_render (s_window_egl *window)
 {
   s_sequence_egl *seq;
   const s_rgb text_color[2] = {
@@ -195,7 +195,7 @@ bool window_egl_demo_render (s_window_egl *window)
   return true;
 }
 
-bool window_egl_demo_resize (s_window_egl *window, u64 w, u64 h)
+u8 window_egl_demo_resize (s_window_egl *window, u64 w, u64 h)
 {
   assert(window);
   (void) window;
