f1b9589880adeb8dc6d746f616c82edbedd3dc85
Perle <56500403+lpinf-code@users.noreply.github.com>
2025-10-28 22:18:26 +0100

    chore: added header for pdf_draw.h

--- pdf/pdf_draw.h
+++ pdf/pdf_draw.h
@@ -1,1 +1,16 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#ifndef KC3_PDF_DRAW_H
+#define KC3_PDF_DRAW_H
 
+#endif /* KC3_PDF_DRAW_H */
