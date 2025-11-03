6e5f77a96092b6d15def514c475f048a60d72a3d
Thomas de Grivel <thoxdg@gmail.com>
2025-11-03 03:42:57 +0100

    fix configure on msys2 clang64

---
+++ crt.c
@@ -0,0 +1,27 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include <stdlib.h>
+
+extern int main (int argc, char** argv);
+
+extern int    __argc _CRTIMP;
+extern char **__argv _CRTIMP;
+
+void mainCRTStartup (void)
+{
+  int ret = main(__argc, __argv);
+  exit(ret);
+}
+
+void __main (void) {
+}
