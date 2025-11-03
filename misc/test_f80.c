4da0432f9daf1862be353ad90aca9790200c1135
Thomas de Grivel <thoxdg@gmail.com>
2025-11-03 16:01:44 +0100

    fix configure HAVE_F80

---
+++ misc/test_f80.c
@@ -0,0 +1,19 @@
+/* kc3
+ * Copyright from 2022 to 2025 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted the above
+ * copyright notice and this permission paragraph are included in all
+ * copies and substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+#include <stdio.h>
+
+int main(void) {
+    printf("sizeof(double) = %zu\n", sizeof(double));
+    printf("sizeof(long double) = %zu\n", sizeof(long double));
+    return 0;
+}
