66d15087f6afd3a561ea1bcd3d89311bad61f1c8
Thomas de Grivel <thoxdg@gmail.com>
2025-12-03 11:20:00 +0100

    sort functions

--- pdf/pdf_stream.c
+++ pdf/pdf_stream.c
@@ -17,46 +17,28 @@
 #include "pdf_stream.h"
 #include "pdf_write.h"
 
-s_pdf_stream * pdf_stream_init (s_pdf_stream *stream)
-{
-  s_pdf_stream tmp = {0};
-  s_buf *buf;
-  if (! stream)
-    return NULL;
-  if (! (buf = buf_new_alloc(BUF_SIZE)))
-    return NULL;
-  tmp.buf = buf;
-  *stream = tmp;
-  return stream;
-}
-
 void pdf_stream_clean (s_pdf_stream *stream)
 {
   map_clean(&stream->dictionnary);
   buf_clean(stream->buf);
 }
 
-p_pdf_stream * pdf_stream_new (p_pdf_stream *dest)
+sw pdf_stream_color_set_cmyk(s_pdf_stream *stream,
+                             s_array *color, bool sb)
 {
-  p_pdf_stream tmp;
-  if (! (tmp = alloc(sizeof(s_pdf_stream))))
-    return NULL;
-  if (! pdf_stream_init(tmp))
-    return NULL;
-  *dest = tmp;
-  return dest;
+  assert(stream);
+  assert(stream->buf);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_flat_array(stream->buf, color)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf,
+                                     sb ? " K" : " k", true)) < 0)
+    return r;
+  result += r;
+  return result;
 }
-
-void pdf_stream_destroy (p_pdf_stream *stream)
-{
-  if (! stream || ! *stream)
-    return;
-  pdf_stream_clean(*stream);
-  free(*stream);
-  *stream = NULL;
-}
-
-/* Color operations */
 
 sw pdf_stream_color_set_color(s_pdf_stream *stream,
                               s_array *color, bool sb)
@@ -109,23 +91,6 @@ sw pdf_stream_color_set_rgb(s_pdf_stream *stream,
   return result;
 }
 
-sw pdf_stream_color_set_cmyk(s_pdf_stream *stream,
-                             s_array *color, bool sb)
-{
-  assert(stream);
-  assert(stream->buf);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_flat_array(stream->buf, color)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf,
-                                     sb ? " K" : " k", true)) < 0)
-    return r;
-  result += r;
-  return result;
-}
-
 sw pdf_stream_color_use_shading(s_pdf_stream *stream, p_sym resource)
 {
   assert(stream);
@@ -141,238 +106,76 @@ sw pdf_stream_color_use_shading(s_pdf_stream *stream, p_sym resource)
   return result;
 }
 
-/* Text operations */
-
-sw pdf_stream_text_begin (s_pdf_stream *stream)
+void pdf_stream_delete (p_pdf_stream *stream)
 {
-  assert(stream);
-  assert(stream->buf);
-  return pdf_buf_write_token_clean(stream->buf, "BT", true);
+  if (! stream || ! *stream)
+    return;
+  pdf_stream_clean(*stream);
+  free(*stream);
+  *stream = NULL;
 }
 
-sw pdf_stream_text_set_font (s_pdf_stream *stream,
-                             p_sym font, s32 size)
-{
-  sw r = 0;
-  sw result = 0;
-  assert(stream);
-  assert(stream->buf);
-  if ((r = pdf_buf_write_name(stream->buf, font)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_integer(stream->buf, size)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
-    return r;
-  }
-  result += r;
-  return result + pdf_buf_write_token_clean(stream->buf, "Tf", true);
-}
-
-sw pdf_stream_text_set_pos_xy (s_pdf_stream *stream, s_pdf_vec2 *pos)
-{
-  sw r = 0;
-  sw result = 0;
-  assert(stream);
-  assert(stream->buf);
-  assert(pos);
-  if ((r = pdf_buf_write_integer(stream->buf, pos->x)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_integer(stream->buf, pos->y)) < 0) {
-    return r;
-  }
-  result += r;
-  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
-    return r;
-  }
-  result += r;
-  return result + pdf_buf_write_token_clean(stream->buf, "Td", true);
-}
-
-// REVIEW NOTES:
-// The lack of separator between the string and "Tj" is normal here
-// as the tokens can't collide (because of the string's bounding
-// characters)
-sw pdf_stream_text_set_text (s_pdf_stream *stream, s_str *text)
-{
-  sw r = 0;
-  assert(stream);
-  assert(stream->buf);
-  if ((r = pdf_buf_write_string_hex(stream->buf, text)) < 0) {
-    return r;
-  }
-  return r + pdf_buf_write_token_clean(stream->buf, "Tj", true);
-}
-
-// REVIEW NOTES:
-// This expects a list of only integers and strings, where each
-// integer indicates the horizontal (x) displacement of the next
-// string, for things like kerning. This is TJ, while the simple
-// string is Tj.
-sw pdf_stream_text_set_text_array (s_pdf_stream *stream,
-                                   const p_list text_array)
-{
-  sw r = 0;
-  assert(stream);
-  assert(stream->buf);
-  if ((r = pdf_buf_write_array(stream->buf, text_array)) < 0) {
-    return r;
-  }
-  return r + pdf_buf_write_token_clean(stream->buf, "TJ", true);
-}
-
-sw pdf_stream_text_end (s_pdf_stream *stream)
-{
-  assert(stream);
-  assert(stream->buf);
-  return pdf_buf_write_token_clean(stream->buf, "BT", true);
-}
-
-/* Image operations */
-
-void pdf_stream_image(void) {}
-
-/* Draw operations */
-
-// q Q
-sw pdf_stream_draw_stack_save(s_pdf_stream *stream)
-{
-  assert(stream);
-  assert(stream->buf);
-  return pdf_buf_write_token_clean(stream->buf, "q", true);
-}
-
-sw pdf_stream_draw_stack_load(s_pdf_stream *stream)
-{
-  assert(stream);
-  assert(stream->buf);
-  return pdf_buf_write_token_clean(stream->buf, "Q", true);
-}
-
-// cm w J j M d
-sw pdf_stream_draw_set_transformation_matrix(s_pdf_stream *stream,
-                                             s_array *m)
+sw pdf_stream_draw_clip(s_pdf_stream *stream, bool even_odd)
 {
   assert(stream);
   assert(stream->buf);
   sw r;
   sw result = 0;
-  if ((r = pdf_buf_write_flat_array(stream->buf, m)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " cm", true)) < 0)
+  if ((r = pdf_buf_write_token_clean(stream->buf,
+      even_odd ? "W*" : "W", true)) < 0)
     return r;
   result += r;
   return result;
 }
 
-sw pdf_stream_draw_set_line_width(s_pdf_stream *stream, f32 width)
+sw pdf_stream_draw_paint(s_pdf_stream *stream,
+                         bool stroke,
+                         enum pdf_fill_mode fill)
 {
   assert(stream);
   assert(stream->buf);
   sw r;
   sw result = 0;
-  if ((r = pdf_buf_write_float(stream->buf, width)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " w", true)) < 0)
-    return r;
-  result += r;
+  if (fill != PDF_FILL_NONE) {
+  assert(stream);
+  assert(stream->buf);
+    if ((r = pdf_buf_write_token_clean(stream->buf,
+        (fill == PDF_FILL_EVEN_ODD) ? "f*" : "f", true)) < 0)
+      return r;
+    result += r;
+  }
+  if (stroke) {
+  assert(stream);
+  assert(stream->buf);
+    if ((r = pdf_buf_write_token_clean(stream->buf, "S", true)) < 0)
+      return r;
+    result += r;
+  }
   return result;
 }
 
-sw pdf_stream_draw_set_line_cap(s_pdf_stream *stream,
-                                enum pdf_line_cap cap)
+sw pdf_stream_draw_paint_close(s_pdf_stream *stream,
+                               bool stroke,
+                               enum pdf_fill_mode fill)
 {
   assert(stream);
   assert(stream->buf);
   sw r;
   sw result = 0;
-  if ((r = pdf_buf_write_integer(stream->buf, (s32)cap)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " J", true)) < 0)
-    return r;
-  result += r;
+  if (fill != PDF_FILL_NONE) {
+    if ((r = pdf_buf_write_token_clean(stream->buf,
+        (fill == PDF_FILL_EVEN_ODD) ? "B*" : "B", true)) < 0)
+      return r;
+    result += r;
+  }
+  if (stroke) {
+    if ((r = pdf_buf_write_token_clean(stream->buf, "b", true)) < 0)
+      return r;
+    result += r;
+  }
   return result;
 }
 
-sw pdf_stream_draw_set_line_join(s_pdf_stream *stream,
-                                 enum pdf_line_join join)
-{
-  assert(stream);
-  assert(stream->buf);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_integer(stream->buf, (s32)join)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " j", true)) < 0)
-    return r;
-  result += r;
-  return result;
-}
-
-sw pdf_stream_draw_set_line_miter_limit(s_pdf_stream *stream,
-                                        f32 limit)
-{
-  assert(stream);
-  assert(stream->buf);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_float(stream->buf, limit)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " M", true)) < 0)
-    return r;
-  result += r;
-  return result;
-}
-
-sw pdf_stream_draw_set_line_dash_pattern(s_pdf_stream *stream,
-                                         s_array *array,
-                                         s32 phase)
-{
-  assert(stream);
-  assert(stream->buf);
-  assert(array);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_token(stream->buf, "[")) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_flat_array(stream->buf, array)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token(stream->buf, "]")) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_integer(stream->buf, phase)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " d", true)) < 0)
-    return r;
-  result += r;
-  return result;
-}
-
-// m l c v y h re
 sw pdf_stream_draw_path_begin_at(s_pdf_stream *stream, s_pdf_vec2 *pos)
 {
   assert(stream);
@@ -384,22 +187,6 @@ sw pdf_stream_draw_path_begin_at(s_pdf_stream *stream, s_pdf_vec2 *pos)
     return r;
   result += r;
   if ((r = pdf_buf_write_token_clean(stream->buf, " m", true)) < 0)
-    return r;
-  result += r;
-  return result;
-}
-
-sw pdf_stream_draw_path_move_to(s_pdf_stream *stream, s_pdf_vec2 *pos)
-{
-  assert(stream);
-  assert(stream->buf);
-  assert(pos);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_vec2(stream->buf, pos)) < 0)
-    return r;
-  result += r;
-  if ((r = pdf_buf_write_token_clean(stream->buf, " l", true)) < 0)
     return r;
   result += r;
   return result;
@@ -497,6 +284,22 @@ sw pdf_stream_draw_path_end_at(s_pdf_stream *stream, s_pdf_vec2 *pos)
   return result;
 }
 
+sw pdf_stream_draw_path_move_to(s_pdf_stream *stream, s_pdf_vec2 *pos)
+{
+  assert(stream);
+  assert(stream->buf);
+  assert(pos);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_vec2(stream->buf, pos)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " l", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
 sw pdf_stream_draw_path_rect(s_pdf_stream *stream, s_pdf_rect *rect)
 {
   assert(stream);
@@ -531,70 +334,129 @@ sw pdf_stream_draw_path_rect(s_pdf_stream *stream, s_pdf_rect *rect)
   return result;
 }
 
-// S s f f* B B* b b* n
-sw pdf_stream_draw_paint(s_pdf_stream *stream,
-                         bool stroke,
-                         enum pdf_fill_mode fill)
+sw pdf_stream_draw_set_line_cap(s_pdf_stream *stream,
+                                enum pdf_line_cap cap)
 {
   assert(stream);
   assert(stream->buf);
   sw r;
   sw result = 0;
-  if (fill != PDF_FILL_NONE) {
-  assert(stream);
-  assert(stream->buf);
-    if ((r = pdf_buf_write_token_clean(stream->buf,
-        (fill == PDF_FILL_EVEN_ODD) ? "f*" : "f", true)) < 0)
-      return r;
-    result += r;
-  }
-  if (stroke) {
-  assert(stream);
-  assert(stream->buf);
-    if ((r = pdf_buf_write_token_clean(stream->buf, "S", true)) < 0)
-      return r;
-    result += r;
-  }
-  return result;
-}
-
-sw pdf_stream_draw_paint_close(s_pdf_stream *stream,
-                               bool stroke,
-                               enum pdf_fill_mode fill)
-{
-  assert(stream);
-  assert(stream->buf);
-  sw r;
-  sw result = 0;
-  if (fill != PDF_FILL_NONE) {
-    if ((r = pdf_buf_write_token_clean(stream->buf,
-        (fill == PDF_FILL_EVEN_ODD) ? "B*" : "B", true)) < 0)
-      return r;
-    result += r;
-  }
-  if (stroke) {
-    if ((r = pdf_buf_write_token_clean(stream->buf, "b", true)) < 0)
-      return r;
-    result += r;
-  }
-  return result;
-}
-
-// W W*
-sw pdf_stream_draw_clip(s_pdf_stream *stream, bool even_odd)
-{
-  assert(stream);
-  assert(stream->buf);
-  sw r;
-  sw result = 0;
-  if ((r = pdf_buf_write_token_clean(stream->buf,
-      even_odd ? "W*" : "W", true)) < 0)
+  if ((r = pdf_buf_write_integer(stream->buf, (s32)cap)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " J", true)) < 0)
     return r;
   result += r;
   return result;
 }
 
-// Do
+sw pdf_stream_draw_set_line_dash_pattern(s_pdf_stream *stream,
+                                         s_array *array,
+                                         s32 phase)
+{
+  assert(stream);
+  assert(stream->buf);
+  assert(array);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_token(stream->buf, "[")) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_flat_array(stream->buf, array)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token(stream->buf, "]")) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_integer(stream->buf, phase)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " d", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
+sw pdf_stream_draw_set_line_join(s_pdf_stream *stream,
+                                 enum pdf_line_join join)
+{
+  assert(stream);
+  assert(stream->buf);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_integer(stream->buf, (s32)join)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " j", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
+sw pdf_stream_draw_set_line_miter_limit(s_pdf_stream *stream,
+                                        f32 limit)
+{
+  assert(stream);
+  assert(stream->buf);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_float(stream->buf, limit)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " M", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
+sw pdf_stream_draw_set_line_width(s_pdf_stream *stream, f32 width)
+{
+  assert(stream);
+  assert(stream->buf);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_float(stream->buf, width)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " w", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
+sw pdf_stream_draw_set_transformation_matrix(s_pdf_stream *stream,
+                                             s_array *m)
+{
+  assert(stream);
+  assert(stream->buf);
+  sw r;
+  sw result = 0;
+  if ((r = pdf_buf_write_flat_array(stream->buf, m)) < 0)
+    return r;
+  result += r;
+  if ((r = pdf_buf_write_token_clean(stream->buf, " cm", true)) < 0)
+    return r;
+  result += r;
+  return result;
+}
+
+sw pdf_stream_draw_stack_load(s_pdf_stream *stream)
+{
+  assert(stream);
+  assert(stream->buf);
+  return pdf_buf_write_token_clean(stream->buf, "Q", true);
+}
+
+sw pdf_stream_draw_stack_save(s_pdf_stream *stream)
+{
+  assert(stream);
+  assert(stream->buf);
+  return pdf_buf_write_token_clean(stream->buf, "q", true);
+}
+
 sw pdf_stream_external_object(s_pdf_stream *stream, p_sym name)
 {
   assert(stream);
@@ -608,4 +470,119 @@ sw pdf_stream_external_object(s_pdf_stream *stream, p_sym name)
     return r;
   result += r;
   return result;
+}
+
+void pdf_stream_image(void) {}
+
+s_pdf_stream * pdf_stream_init (s_pdf_stream *stream)
+{
+  s_pdf_stream tmp = {0};
+  s_buf *buf;
+  if (! stream)
+    return NULL;
+  if (! (buf = buf_new_alloc(BUF_SIZE)))
+    return NULL;
+  tmp.buf = buf;
+  *stream = tmp;
+  return stream;
+}
+
+p_pdf_stream * pdf_stream_new (p_pdf_stream *dest)
+{
+  p_pdf_stream tmp;
+  if (! (tmp = alloc(sizeof(s_pdf_stream))))
+    return NULL;
+  if (! pdf_stream_init(tmp))
+    return NULL;
+  *dest = tmp;
+  return dest;
+}
+
+sw pdf_stream_text_begin (s_pdf_stream *stream)
+{
+  assert(stream);
+  assert(stream->buf);
+  return pdf_buf_write_token_clean(stream->buf, "BT", true);
+}
+
+sw pdf_stream_text_end (s_pdf_stream *stream)
+{
+  assert(stream);
+  assert(stream->buf);
+  return pdf_buf_write_token_clean(stream->buf, "BT", true);
+}
+
+sw pdf_stream_text_set_font (s_pdf_stream *stream,
+                             p_sym font, s32 size)
+{
+  sw r = 0;
+  sw result = 0;
+  assert(stream);
+  assert(stream->buf);
+  if ((r = pdf_buf_write_name(stream->buf, font)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_integer(stream->buf, size)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
+    return r;
+  }
+  result += r;
+  return result + pdf_buf_write_token_clean(stream->buf, "Tf", true);
+}
+
+sw pdf_stream_text_set_pos_xy (s_pdf_stream *stream, s_pdf_vec2 *pos)
+{
+  sw r = 0;
+  sw result = 0;
+  assert(stream);
+  assert(stream->buf);
+  assert(pos);
+  if ((r = pdf_buf_write_integer(stream->buf, pos->x)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_integer(stream->buf, pos->y)) < 0) {
+    return r;
+  }
+  result += r;
+  if ((r = pdf_buf_write_separator(stream->buf, false)) < 0) {
+    return r;
+  }
+  result += r;
+  return result + pdf_buf_write_token_clean(stream->buf, "Td", true);
+}
+
+sw pdf_stream_text_set_text (s_pdf_stream *stream, s_str *text)
+{
+  sw r = 0;
+  assert(stream);
+  assert(stream->buf);
+  if ((r = pdf_buf_write_string_hex(stream->buf, text)) < 0) {
+    return r;
+  }
+  return r + pdf_buf_write_token_clean(stream->buf, "Tj", true);
+}
+
+sw pdf_stream_text_set_text_array (s_pdf_stream *stream,
+                                   const p_list text_array)
+{
+  sw r = 0;
+  assert(stream);
+  assert(stream->buf);
+  if ((r = pdf_buf_write_array(stream->buf, text_array)) < 0) {
+    return r;
+  }
+  return r + pdf_buf_write_token_clean(stream->buf, "TJ", true);
 }
