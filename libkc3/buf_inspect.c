0ea6c1bf99c3c1b173fed2f9f3033dfa32bb1bb9
Thomas de Grivel <thoxdg@gmail.com>
2025-12-11 12:24:30 +0100

    fix buf_inspect_list_size

--- libkc3/buf_inspect.c
+++ libkc3/buf_inspect.c
@@ -3044,14 +3044,8 @@ sw buf_inspect_list_size (s_pretty *pretty, const s_list *x)
     switch (i->next.type) {
     case TAG_PLIST:
       if (i->next.data.plist) {
-        if (alist) {
-          if ((r = buf_write_1_size(pretty, ",\n")) < 0)
-            return r;
-        }
-        else {
-          if ((r = buf_write_1_size(pretty, ", ")) < 0)
-            return r;
-        }
+        if ((r = buf_write_1_size(pretty, ",\n")) < 0)
+          return r;
         result += r;
       }
       i = i->next.data.plist;
